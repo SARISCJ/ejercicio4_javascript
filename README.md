82079b5236767d026c469a972ec2bcb61f529eaa
SARISCJ <saracastillo0@gmail.com>
2016-08-25 15:40:08 -0500

    cuarto ejercicio

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ejercicio 4 
+
+###### Pseudocodigo  
+
+1. Inicio
+2. Leer datos
+	Dato1
+	Dato2
+3. Multiplicar datos
+4. Entregar datos
+5. Fin 
+
+### Diagrama de flujo
+
+![Sin titulo](http://i63.tinypic.com/2q01qx5.jpg)
---
+++ js/app.js
@@ -0,0 +1,4 @@
+var minutos = prompt("digite los minutos que demoro la llamada");
+var costo = prompt("pago por minuto");
+var total = parseInt(minutos) * parseInt(costo);
+window.alert ("total a pagar por su llamada es: " + total + " Nuevos Soles");
